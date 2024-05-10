396fcb83dcc89c27e5f50715d463a492e076de7c
Jacob Peterson <petersonjacobw@gmail.com>
2024-05-10 22:29:06 +0100

    Adds additional resources and updates formatting

--- client/src/components/Help/Resources.jsx
+++ client/src/components/Help/Resources.jsx
@@ -1,18 +1,71 @@
 import React from 'react';
 import resources from '../../libraries/resources.js';
 import {
-  HelpSection, HelpText, StyledH1, StyledNavLink, PageWrapper,
+  HelpSection, HelpText, ResourceNameAndCreator, StyledH1, StyledH2, StyledNavLink, StyledSpan, PageWrapper,
 } from '../../styles.js';
 
 const Resources = () => (
-    <PageWrapper flexDirection="column">
+  <PageWrapper flexDirection="column">
     <HelpSection id="resources">
-      <StyledH1 textAlign="center">External resources</StyledH1>
-      {resources.map(({name, creator, description, url}, index) => (
+      <StyledH1 textAlign="center">Online resources</StyledH1>
+      <StyledH2 color="#333" textAlign="left">Overviews</StyledH2>
+      {resources.introductions.map(({ name, creator, description, journal, journalDetails, url }, index) => (
         <>
-          <StyledNavLink target="_blank" href={url} color="#3e5276" fontSize={24}>{name}</StyledNavLink>
-          {creator && <span>{`by ${creator}`}</span>}
-          {description && <HelpText>{description}</HelpText>}
+          <ResourceNameAndCreator>
+            <StyledNavLink target="_blank" href={url} color="#3e5276" fontSize={24}>{name}</StyledNavLink>
+            {creator && <StyledSpan fontSize={14}>{`by ${creator}`}</StyledSpan>}
+          </ResourceNameAndCreator>
+          {(description || journal) && (
+            <HelpText>
+              {journal && (<><i>{`${journal}`}</i>{`${journalDetails}`}<br /></>)}
+              {description && <>{description}</>}
+            </HelpText>
+          )}
+        </>
+      ))}
+      <StyledH2 color="#333" textAlign="left">Reading aids</StyledH2>
+      {resources.helps.map(({ name, creator, description, journal, journalDetails, url }, index) => (
+        <>
+          <ResourceNameAndCreator>
+            <StyledNavLink target="_blank" href={url} color="#3e5276" fontSize={24}>{name}</StyledNavLink>
+            {creator && <StyledSpan fontSize={14}>{`by ${creator}`}</StyledSpan>}
+          </ResourceNameAndCreator>
+          {(description || journal) && (
+            <HelpText>
+              {journal && (<><i>{`${journal}`}</i>{`${journalDetails}`}<br /></>)}
+              {description && <>{description}</>}
+            </HelpText>
+          )}
+        </>
+      ))}
+      <StyledH2 color="#333" textAlign="left">Images with transcriptions</StyledH2>
+      {resources.images.map(({ name, creator, description, journal, journalDetails, url }, index) => (
+        <>
+          <ResourceNameAndCreator>
+            <StyledNavLink target="_blank" href={url} color="#3e5276" fontSize={24}>{name}</StyledNavLink>
+            {creator && <StyledSpan fontSize={14}>{`by ${creator}`}</StyledSpan>}
+          </ResourceNameAndCreator>
+          {(description || journal) && (
+            <HelpText>
+              {journal && (<><i>{`${journal}`}</i>{`${journalDetails}`}<br /></>)}
+              {description && <>{description}</>}
+            </HelpText>
+          )}
+        </>
+      ))}
+      <StyledH2 color="#333" textAlign="left">Palaeography</StyledH2>
+      {resources.palaeography.map(({ name, creator, description, journal, journalDetails, url }, index) => (
+        <>
+          <ResourceNameAndCreator>
+            <StyledNavLink target="_blank" href={url} color="#3e5276" fontSize={24}>{name}</StyledNavLink>
+            {creator && <StyledSpan fontSize={14}>{`by ${creator}`}</StyledSpan>}
+          </ResourceNameAndCreator>
+          {(description || journal) && (
+            <HelpText>
+              {journal && (<><i>{`${journal}`}</i>{`${journalDetails}`}<br /></>)}
+              {description && <>{description}</>}
+            </HelpText>
+          )}
         </>
       ))}
     </HelpSection>
