fec77785a2d03dc76a801feec8e25e1ef98d3d6f
Jacob Peterson <petersonjacobw@gmail.com>
2024-05-14 19:34:53 +0100

    chore: Adds typing for Mirador related components

--- src/libraries/manifests.ts
+++ src/libraries/manifests.ts
@@ -1,50 +1,59 @@
 //  Other potential MSS:
 // Aristophanes minuscules https://www.digitale-sammlungen.de/en/view/bsb00069327?page=,1
 
-const manifests = {
+export type Line = {
+  text: string,
+  caption?: string,
+  newConcept?: string,
+  isTitle?: boolean
+}
+
+export type Manifest = {
+  manifestId: string,
+  canvasIndex: number,
+  lines: Line[],
+}
+
+export type Manifests = {
+  [key: string]: Manifest
+}
+
+const manifests: Manifests = {
   1: {
     manifestId: 'https://cudl.lib.cam.ac.uk/iiif/MS-ADD-06594',
     // manifestId: '',
     canvasIndex: 160,
-    title: {
-      text: 'ευαγγελιον κατα μαρκον',
-      key: 'title',
-    },
     lines: [
       {
+        text: 'ευαγγελιον κατα μαρκον',
+        isTitle: true,
+      },
+      {
         text: 'αρχη του ευαγγελιου ιυ χυ υυ του θυ',
-        key: 1,
         newConcept: 'Ekthesis',
         caption: 'The red αρ with superscript χ is shorthand for αρχη and marks the start of a section. Exclude it from your transcription.',
       },
       {
         text: 'ωσγεγραπταιεντοισπροφηταισ',
-        key: 2,
       },
       {
         text: 'ιδουεγωαποστελλωτοναγγε',
-        key: 3,
       },
       {
         text: 'λονμουπροπροσωπουσουοσ',
-        key: 4,
       },
       {
         text: 'κατασκευασειτηνοδονσουεμπρο',
-        key: 5,
       },
       {
         text: 'σθενσουφωνηβοωντοσεντηερημω',
-        key: 6,
         caption: 'You\'ll notice a superscript ω at the end of the line. This is part of the final word.',
       },
       {
         text: 'ετοιμασατετηνοδονκυευθειασποι',
-        key: 7,
       },
       {
         text: 'ειτεταστριβουσαυτουεγενετοιω',
-        key: 8,
       },
     ],
   },
@@ -52,68 +61,53 @@ const manifests = {
     // helpful quasi transcription https://books.google.com/books?id=2c5hAAAAcAAJ&pg=PP118&lpg=PP118&dq=ephrem+syrus+%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82+%CE%BB%CE%BF%CE%B3%CE%BF%CF%82+%CE%B1&source=bl&ots=LeXDEgtDAt&sig=ACfU3U1qoHsw12pFlyLR5Tae8UnNklc6kg&hl=en&sa=X&ved=2ahUKEwif16uHiNH0AhX-Ap0JHUBGA_QQ6AF6BAgYEAM#v=onepage&q=ephrem%20syrus%20%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82%20%CE%BB%CE%BF%CE%B3%CE%BF%CF%82%20%CE%B1&f=false
     manifestId: 'https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00050975/manifest',
     canvasIndex: 156,
-    title: {
-      text: 'λογοσαυτωνκατανυκτικοσλογοσα',
-      key: 'title',
-      newConcept: 'Abbreviation',
-    },
     lines: [
       {
+        text: 'λογοσαυτωνκατανυκτικοσλογοσα',
+        newConcept: 'Abbreviation',
+      },
+      {
         text: 'ενμιαουντωνημερων',
-        key: 1,
       },
       {
         text: 'ορθρουμιαναστασεπορευομην',
-        key: 2,
         newConcept: 'Ligature',
       },
       {
         text: '',
-        key: 3,
       },
       {
         text: '',
-        key: 4,
       },
       {
         text: '',
-        key: 5,
       },
       {
         text: '',
-        key: 6,
       },
       {
         text: '',
-        key: 7,
       },
       {
         text: '',
-        key: 8,
       },
       {
         text: '',
-        key: 9,
       },
       {
         text: '',
-        key: 10,
       },
       {
         text: '',
-        key: 11,
       },
       {
         text: '',
-        key: 12,
       },
       {
         text: '',
-        key: 13,
       },
       {
         text: '',
-        key: 14,
       },
     ],
   },
@@ -123,36 +117,28 @@ const manifests = {
     lines: [
       {
         text: 'αρχητουευαγγελιουιυχυυυτουθυ',
-        key: 1,
         newConcept: 'Ekthesis',
       },
       {
         text: 'ωσγεγραπταιεντοισπροφηταισ',
-        key: 2,
       },
       {
         text: 'αρχητουευαγγελιουιυχυυυτουθυ',
-        key: 3,
       },
       {
         text: 'ωσγεγραπταιεντοισπροφηταισ',
-        key: 4,
       },
       {
         text: 'αρχητουευαγγελιουιυχυυυτουθυ',
-        key: 5,
       },
       {
         text: 'ωσγεγραπταιεντοισπροφηταισ',
-        key: 6,
       },
       {
         text: 'αρχητουευαγγελιουιυχυυυτουθυ',
-        key: 7,
       },
       {
         text: 'ωσγεγραπταιεντοισπροφηταισ',
-        key: 8,
       },
     ],
   },
