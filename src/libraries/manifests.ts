164142b70b5ab53316e84867ce0dbf3c858d2ec5
Jacob Peterson <petersonjacobw@gmail.com>
2024-05-19 23:34:12 +0100

    feat: new manifests added

--- src/libraries/manifests.ts
+++ src/libraries/manifests.ts
@@ -24,38 +24,97 @@ const manifests: Manifests = {
     manifestId: "https://digi.vatlib.it/iiif/MSS_Vat.gr.1209/manifest.json",
     canvasIndex: 773,
     instruction:
-      "This lesson is about the right column. Treat indented lines as part of the preceding line when entering them in the forms.",
+      "This lesson is about the right column. The title is above the green rectangle. Treat indented lines as part of the preceding line when entering them in the forms.",
     lines: [
       {
-        text: "αρχητουευαγγελιουιυχυυυτουθυ",
+        text: "ιωβ",
+        isTitle: true,
+      },
+      {
+        text: "ανθρωποσ τισ ην εν χωρα τη αυσιτιδι ω ονοµα ιωβ",
         newConcept: "Ekthesis",
+        caption:
+          "The new concept (click the NC icon) was made possible by the erasure of the first letter on this line.",
       },
       {
-        text: "ωσγεγραπταιεντοισπροφηταισ",
+        text: "και ην ο ανθρωποσ εκεινοσ αληθινοσ αµεµπτοσ δικαιοσ θεοσεβησ",
       },
       {
-        text: "αρχητουευαγγελιουιυχυυυτουθυ",
+        text: "απεχοµενοσ απο παντοσ πονηρου πραγµατοσ",
       },
       {
-        text: "ωσγεγραπταιεντοισπροφηταισ",
+        text: "εγενοντο δε αυτω υιοι επτα και θυγατερεσ τρεισ",
       },
       {
-        text: "αρχητουευαγγελιουιυχυυυτουθυ",
+        text: "και ην τα κτηνη αυτου προβατα επτακισχιλια",
       },
       {
-        text: "ωσγεγραπταιεντοισπροφηταισ",
+        text: "καµηλοι τρισχιλιαι",
       },
       {
-        text: "αρχητουευαγγελιουιυχυυυτουθυ",
+        text: "ζευγη βοων πεντακοσια",
       },
       {
-        text: "ωσγεγραπταιεντοισπροφηταισ",
+        text: "ονοι θηλειαι νοµαδεσ πεντακοσιαι",
+      },
+      {
+        text: "και υπηρεσια πολλη σφοδρα",
+      },
+      {
+        text: "και εργα µεγαλα ην αυτω επι τησ γησ",
+      },
+      {
+        text: "και ην ο ανθρωποσ εκεινοσ ευγενησ των αφ ηλιου ανατολων",
+        newConcept: "Final nu",
+      },
+      {
+        text: "συµπορευοµενοι δε οι υιοι αυτου προσ αλληλουσ",
+      },
+      {
+        text: "εποιουσαν ποτον καθ εκαστην ηµεραν",
+      },
+      {
+        text: "συµπαραλαµβανοντεσ αµα και τασ τρεισ αδελφασ αυτων",
+      },
+      {
+        text: "εσθιειν και πινειν µετ αυτων",
+      },
+      {
+        text: "και ωσ αν συνετελεσθησαν αι ηµεραι του ποτου",
+      },
+      {
+        text: "απεστελλεν ιωβ και εκαθαριζεν αυτουσ ανισταµενοσ το πρωι",
+      },
+      {
+        text: "και προσεφερεν περι αυτων θυσιαν κατα τον αριθµον αυτων",
+      },
+      {
+        text: "και µοσχον ενα περι αµαρτιασ περι των ψυχων αυτων",
+      },
+      {
+        text: "ελεγεν γαρ ιωβ µηποτε οι υιοι µου εν τη διανοια αυτων κακα ενενοησαν προσ θν",
+        newConcept: "Nomina sacra",
+      },
+      {
+        text: "ουτωσ ουν εποιει ιωβ πασασ τασ ηµερασ",
+      },
+      {
+        text: "και ωσ εγενετο η ηµερα αυτη",
+      },
+      {
+        text: "και ιδου ηλθον οι αγγελοι του θυ παραστηναι ενωπιον του κυ",
+      },
+      {
+        text: "και ο διαβολοσ ηλθεν µετ αυτων",
+        caption: "Include the faded nu in your transcription.",
       },
     ],
   },
   2: {
     manifestId: "https://cudl.lib.cam.ac.uk/iiif/MS-ADD-06594",
     canvasIndex: 161,
+    instruction:
+      "No new concepts in this lesson, just more examples of those learned in the previous lesson but in a different style of majuscule script.",
     lines: [
       {
         text: "ευαγγελιον κατα μαρκον",
@@ -63,7 +122,6 @@ const manifests: Manifests = {
       },
       {
         text: "αρχη του ευαγγελιου ιυ χυ υυ του θυ",
-        newConcept: "Nomina sacra",
         caption:
           "The red αρ with superscript χ is shorthand for αρχη and marks the start of a section. Exclude it from your transcription.",
       },
@@ -93,14 +151,83 @@ const manifests: Manifests = {
     ],
   },
   3: {
-    // helpful quasi transcription https://books.google.com/books?id=2c5hAAAAcAAJ&pg=PP118&lpg=PP118&dq=ephrem+syrus+%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82+%CE%BB%CE%BF%CE%B3%CE%BF%CF%82+%CE%B1&source=bl&ots=LeXDEgtDAt&sig=ACfU3U1qoHsw12pFlyLR5Tae8UnNklc6kg&hl=en&sa=X&ved=2ahUKEwif16uHiNH0AhX-Ap0JHUBGA_QQ6AF6BAgYEAM#v=onepage&q=ephrem%20syrus%20%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82%20%CE%BB%CE%BF%CE%B3%CE%BF%CF%82%20%CE%B1&f=false
+    manifestId:
+      "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00058840/manifest",
+    canvasIndex: 53,
+    instruction:
+      "This lesson features relatively easy to read minuscule text but focuses on a page of kephalaia (ancient chapter divisions). Each line contains a Greek number, then an abbreviation for περι, and then a short title for that section. Treat lines that do not begin with a number as part of the preceding line.",
+    lines: [
+      {
+        text: "λυσαι την γυναικα αυτου",
+      },
+      {
+        text: "μα περι του επερωτησαντοσ πλουσιου τον ιν",
+        newConcept: "Numbers",
+      },
+      {
+        text: "μβ περι των μισθουμενων εργατων",
+      },
+      {
+        text: "μγ περι των υιων ζεβεδαιου",
+      },
+      {
+        text: "μδ περι των δυο τυφλων",
+      },
+      {
+        text: "με περι τησ ονου και του πωλου",
+      },
+      {
+        text: "μϛ περι των τυφλων και των χωλων",
+        newConcept: "Stigma",
+      },
+      {
+        text: "μζ περι τησ ξηρανθησησ συκησ",
+      },
+      {
+        text: "μη περι των επερωτησαντων τον κν αρχιερεων και πρεσβυτερων",
+      },
+      {
+        text: "μθ περι των δυο υιων παραβολη",
+      },
+      {
+        text: "ν περι του αμπελωνοσ παραβολη",
+        caption: "This line contains a tricky abbreviation.",
+      },
+      {
+        text: "να περι των καλουμενων εισ τον γαμον",
+      },
+      {
+        text: "νβ περι των επερωτησαντων δια τον κηνσον",
+      },
+      {
+        text: "νγ περι των σαδδουκαιων",
+      },
+      {
+        text: "νδ περι του νομικου",
+      },
+      {
+        text: "νε περι τησ του κυ επερωτησεωσ προσ τεισ φαρισαιουσ",
+        caption:
+          "This line contains an error or confuses abbreviations that makes it too difficult. Near the end of the line is προ followed by a στ ligature with two squiggles above it. This represents προς τεις even though it grammatically should have been προς τους.",
+      },
+      {
+        text: "νϛ περι του ταλανισμου των γραμματαιων και φαρισαιων",
+        caption:
+          "The final vertical stroke and long squiggly line at the end of the first line are an abbreviation.",
+      },
+      {
+        text: "νζ περι τησ συντελειασ",
+      },
+    ],
+  },
+  4: {
+    // helpful edition https://books.google.com/books?id=2c5hAAAAcAAJ&pg=PP118&lpg=PP118&dq=ephrem+syrus+%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82+%CE%BB%CE%BF%CE%B3%CE%BF%CF%82+%CE%B1&source=bl&ots=LeXDEgtDAt&sig=ACfU3U1qoHsw12pFlyLR5Tae8UnNklc6kg&hl=en&sa=X&ved=2ahUKEwif16uHiNH0AhX-Ap0JHUBGA_QQ6AF6BAgYEAM#v=onepage&q=ephrem%20syrus%20%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82%20%CE%BB%CE%BF%CE%B3%CE%BF%CF%82%20%CE%B1&f=false
     manifestId:
       "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00050975/manifest",
     canvasIndex: 157,
     lines: [
       {
         text: "λογοσ αυτων κατανυκτικοσ λογοσ α",
-        newConcept: "Abbreviation",
         isTitle: true,
       },
       {
@@ -111,40 +238,41 @@ const manifests: Manifests = {
         newConcept: "Ligature",
       },
       {
-        text: "",
+        text: "εγω συν τοισ δυο αδελφοισ εξωθεν",
+        newConcept: "Abbreviation",
       },
       {
-        text: "",
+        text: "τοισ πολεωσ εδεσων ευλογημενησ",
       },
       {
-        text: "",
+        text: "και ειρα τοισ οφθαλμοισ μου εισ τον",
       },
       {
-        text: "",
+        text: "ουνον ωσ εσοπρον καθορων λαμπον",
       },
       {
-        text: "",
+        text: "τα συν τοισ αστρισ μετα δοξησ επι γησ και",
       },
       {
-        text: "",
+        text: "θαυμασασ ελεγον αν αρα ταυτα ουτοσ λα",
       },
       {
-        text: "",
+        text: "μποσιν μετα δοξησ δικαιοι και αγιοι ποι",
       },
       {
-        text: "",
+        text: "ησαντεσ το θελημα του θυ του αγιου εν",
       },
       {
-        text: "",
+        text: "τη ωρα εκεινη οταν ελθη ο κσ ποσω",
       },
       {
-        text: "",
+        text: "μαιλον λαμψουσιν φωτι ανεκλαλητω",
       },
       {
-        text: "",
+        text: "τησ δοξησ του σρσ παραχριμα δε μνη",
       },
       {
-        text: "",
+        text: "σθησ τησ φοβερασ εκεινησ ελευσεωσ",
       },
     ],
   },
