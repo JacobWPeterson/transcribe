5813934e8d86174b397558e86e5585af51f54b8f
Jacob Peterson <petersonjacobw@gmail.com>
2024-07-05 20:19:59 +0100

    test: Adds tests for Home, E404, and Help pages

---
+++ src/features/Home/Home.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+
+import { Home } from "./Home";
+
+describe("Home", () => {
+  it("should render correctly", () => {
+    render(<Home />);
+
+    expect(screen.getByText("Guided lessons")).toBeInTheDocument();
+    expect(screen.getByText("for learning to read")).toBeInTheDocument();
+    expect(screen.getByText("Greek manuscripts")).toBeInTheDocument();
+
+    expect(screen.getByText("Features")).toBeInTheDocument();
+    expect(screen.getAllByRole("listitem")).toHaveLength(9);
+
+    expect(screen.getByRole("button", { name: "Get started" })).toBeEnabled();
+  });
+});
