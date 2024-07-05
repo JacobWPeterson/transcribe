adfbc35825900537ecf0a52118b65513789211ca
Jacob Peterson <petersonjacobw@gmail.com>
2024-07-05 20:19:59 +0100

    test: Adds tests for Home, E404, and Help pages

---
+++ src/features/Help/Glossary/GlossaryEntry/GlossaryEntry.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+
+import { GlossaryEntry } from "./GlossaryEntry";
+
+describe("GlossaryEntry", () => {
+  it("should render correctly", () => {
+    render(<GlossaryEntry word={"Term"} gloss={"Here is a gloss."} />);
+
+    expect(
+      screen.getByRole("heading", { level: 4, name: "Term" })
+    ).toBeInTheDocument();
+    expect(screen.getByText("Here is a gloss.")).toBeInTheDocument();
+  });
+});
