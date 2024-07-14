d0ddb7c7a961555d41358623e70e7ac723d7bd9b
Jacob Peterson <petersonjacobw@gmail.com>
2024-07-14 08:52:43 +0100

    feat: Recheck answers on toggle of requiring spaces (#100)

--- src/features/Workspace/TranscriptionArea/SingleLine.tsx
+++ src/features/Workspace/TranscriptionArea/SingleLine.tsx
@@ -86,6 +86,15 @@ export const SingleLine = ({
     setHasError(includesNonGreekChars(lineContent));
   }, [lineContent]);
 
+  useEffect(() => {
+    if (!lineContent) {
+      return;
+    }
+    setSubmissionStatus(
+      evaluateSubmission(lineContent, line.text, requireSpaces)
+    );
+  }, [requireSpaces]);
+
   const clearMessages = (): void => {
     setShowHint(false);
     setShowIncorrectErrorMessaging(false);
