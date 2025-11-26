faa8da7b5f12532195b20e2c2c0f1bb3499ab4c4
Jacob Peterson <petersonjacobw@gmail.com>
2025-11-26 23:00:09 +0000

    feat: Add lessons (#308)

--- src/files/manifests.ts
+++ src/files/manifests.ts
@@ -85,6 +85,7 @@ const manifests: Manifests = {
       },
       {
         text: "απεστελλεν ιωβ και εκαθαριζεν αυτουσ ανισταμενοσ το πρωι",
+        newConcept: "Diaeresis",
       },
       {
         text: "και προσεφερεν περι αυτων θυσιαν κατα τον αριθμον αυτων",
@@ -112,52 +113,12 @@ const manifests: Manifests = {
     ],
   },
   2: {
-    manifestId: "https://cudl.lib.cam.ac.uk/iiif/MS-ADD-06594",
-    canvasIndex: 161,
-    instruction:
-      "No new concepts in this lesson, just more examples of those learned in the previous lesson but in a different style of majuscule script.",
-    lines: [
-      {
-        text: "ευαγγελιον κατα μαρκον",
-        isTitle: true,
-      },
-      {
-        text: "αρχη του ευαγγελιου ιυ χυ υυ του θυ",
-        caption:
-          "The red αρ with superscript χ is shorthand for αρχη and marks the start of a section. Exclude it from your transcription.",
-      },
-      {
-        text: "ωσ γεγραπται εν τοισ προφηταισ",
-      },
-      {
-        text: "ιδου εγω αποστελλω τον αγγε",
-      },
-      {
-        text: "λον μου προ προσωπου σου οσ",
-      },
-      {
-        text: "κατασκευασει την οδον σου εμπρο",
-      },
-      {
-        text: "σθεν σου φωνη βοωντοσ εν τη ερημω",
-        caption:
-          "You'll notice a superscript ω at the end of the line. This is part of the final word.",
-      },
-      {
-        text: "ετοιμασατε την οδον κυ ευθειασ ποι",
-      },
-      {
-        text: "ειτε τασ τριβουσ αυτου εγενετο ιω",
-      },
-    ],
-  },
-  3: {
     manifestId:
       "https://viewer.cbl.ie/viewer/api/v1/records/BP_X_f_77/manifest",
     canvasIndex: 1,
     specialIndexHandling: "pages/",
     instruction:
-      "A papyrus manuscript by a trained scribe in a slightly irregular majuscule script. This lesson ends when the papyrus becomes damaged. Ignore the page number (πνγ) at the top of the leaf.",
+      "A papyrus manuscript by a trained scribe in a slightly irregular majuscule script. This lesson ends when the papyrus becomes damaged. Ignore the page number (πνγ) at the top of the leaf. No new concepts here, just new letter forms different the uniform block capitals of the prior manuscript.",
     lines: [
       {
         text: "και ιδου οφθαλμοι ωσει οφθαλ",
@@ -215,77 +176,181 @@ const manifests: Manifests = {
       },
     ],
   },
+  3: {
+    manifestId: "https://cudl.lib.cam.ac.uk/iiif/MS-ADD-06594",
+    canvasIndex: 161,
+    instruction:
+      "No new concepts again in this lesson, just more examples of those learned in the previous lessons but in a different style of majuscule script.",
+    lines: [
+      {
+        text: "ευαγγελιον κατα μαρκον",
+        isTitle: true,
+      },
+      {
+        text: "αρχη του ευαγγελιου ιυ χυ υυ του θυ",
+        caption:
+          "The red αρ with superscript χ is shorthand for αρχη and marks the start of a section. Exclude it from your transcription.",
+      },
+      {
+        text: "ωσ γεγραπται εν τοισ προφηταισ",
+      },
+      {
+        text: "ιδου εγω αποστελλω τον αγγε",
+      },
+      {
+        text: "λον μου προ προσωπου σου οσ",
+      },
+      {
+        text: "κατασκευασει την οδον σου εμπρο",
+      },
+      {
+        text: "σθεν σου φωνη βοωντοσ εν τη ερημω",
+        caption:
+          "You'll notice a superscript ω at the end of the line. This is part of the final word.",
+      },
+      {
+        text: "ετοιμασατε την οδον κυ ευθειασ ποι",
+      },
+      {
+        text: "ειτε τασ τριβουσ αυτου εγενετο ιω",
+      },
+    ],
+  },
   4: {
     manifestId:
       "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00058840/manifest",
-    canvasIndex: 53,
+    canvasIndex: 51,
     instruction:
-      "This lesson features relatively easy to read minuscule text but focuses on a page of kephalaia (ancient chapter divisions). Each line contains a Greek number, then an abbreviation for περι, and then a short title for that section. Treat lines that do not begin with a number as part of the preceding line.",
+      "This lesson features relatively easy to read minuscule text but introduces ligatures, abbreviations, and numbers, so be sure to consult the 'Online Resources' under the Help tab for resources to help intrepret these. This is a page of kephalaia (ancient chapter divisions) where each line contains a Greek number and then a short title for that section. Many lines begin with an abbreviation for περι (the stacked πε).",
     lines: [
       {
-        text: "λυσαι την γυναικα αυτου",
-      },
-      {
-        text: "μα περι του επερωτησαντοσ πλουσιου τον ιν",
+        text: "α περι των μαγων",
         newConcept: "Numbers",
       },
       {
-        text: "μβ περι των μισθουμενων εργατων",
+        text: "β περι των αναιρεθεντων παιδιων",
       },
       {
-        text: "μγ περι των υιων ζεβεδαιου",
+        text: "γ πρωτοσ ιωαννησ εκηρυξεν βασιλειαν ουνω",
+        caption: "The end of this line features a nomen sacrum plus final nu.",
       },
       {
-        text: "μδ περι των δυο τυφλων",
+        text: "δ περι διδασκαλιασ του σρσ",
+        newConcept: "Ligature",
+        caption: "Note the ligature here contains parts of two words.",
       },
       {
-        text: "με περι τησ ονου και του πωλου",
+        text: "ε περι των μακαρισμων",
       },
       {
-        text: "μϛ περι των τυφλων και των χωλων",
+        text: "ϛ περι του λεπρου",
         newConcept: "Stigma",
       },
       {
-        text: "μζ περι τησ ξηρανθησησ συκησ",
+        text: "ζ περι του εκατονταρχου",
       },
       {
-        text: "μη περι των επερωτησαντων τον κν αρχιερεων και πρεσβυτερων",
+        text: "η περι τησ πενθερασ πετρου",
       },
       {
-        text: "μθ περι των δυο υιων παραβολη",
+        text: "θ περι των ιαθεντων απο ποικιλων νοσων",
       },
       {
-        text: "ν περι του αμπελωνοσ παραβολη",
-        caption: "This line contains a tricky abbreviation.",
+        text: "ι περι του μη επιτρεπομενου ακολουθειν",
       },
       {
-        text: "να περι των καλουμενων εισ τον γαμον",
+        text: "ια περι τησ επιτημησεωσ των υδατων",
       },
       {
-        text: "νβ περι των επερωτησαντων δια τον κηνσον",
+        text: "ιβ περι των δαιμονιζωμενων",
       },
       {
-        text: "νγ περι των σαδδουκαιων",
+        text: "ιγ περι του παραλυτικου",
       },
       {
-        text: "νδ περι του νομικου",
+        text: "ιδ περι ματθαιου",
       },
       {
-        text: "νε περι τησ του κυ επερωτησεωσ προσ τεισ φαρισαιουσ",
+        text: "ιε περι τησ θυγατροσ του αρχησυναγωγου",
+        newConcept: "Abbreviation",
+        caption: "This abbreviation results in a variant reading.",
+      },
+      {
+        text: "ιϛ περι τησ αιμορροουσησ",
+      },
+      {
+        text: "ιζ περι των δυο τυφλων",
         caption:
-          "This line contains an error or confuses abbreviations that makes it too difficult. Near the end of the line is προ followed by a στ ligature with two squiggles above it. This represents προς τεις even though it grammatically should have been προς τους.",
+          "Be careful with the letter after δυ; it appears to be damaged.",
       },
       {
-        text: "νϛ περι του ταλανισμου των γραμματαιων και φαρισαιων",
-        caption:
-          "The final vertical stroke and long squiggly line at the end of the first line are an abbreviation.",
+        text: "ιη περι κωφου δαιμονιζομενου",
       },
       {
-        text: "νζ περι τησ συντελειασ",
+        text: "ιθ περι τησ των αποστολων διαταγησ",
+        caption: "The large abbreviation/ligature is for αποστολων",
+      },
+      {
+        text: "κ περι των αποσταλεντων παρα ιωαννου",
       },
     ],
   },
   5: {
+    manifestId: "https://viewer.cbl.ie/viewer/api/v2/records/MP_2_86/manifest",
+    canvasIndex: 2,
+    specialIndexHandling: "pages/",
+    instruction:
+      "This manuscript features more irregular letter forms than you have seen so far. Be particularly careful with letters that might easily be confused for others.",
+    lines: [
+      {
+        text: "κουσατε μου και ζησεται ε",
+      },
+      {
+        text: "ν αγαθοισ η ψυχη υμων και",
+      },
+      {
+        text: "διαθησομαι υμιν διαθη",
+      },
+      {
+        text: "κην αιωνιον τα οσια δαυιδ",
+        caption:
+          "There's a correction on this line after the tau. It looks as if the scribe skipped a letter, starting writing the next word for two letters, then made the correction. Transcribe these two letters as α ο.",
+      },
+      {
+        text: "τα πιστα ιδου μαρτυριαν εν ε",
+      },
+      {
+        text: "θνεσιν αυτον δεδωκα αρχον",
+        caption:
+          "Another correction here by a later scribe; don't include it in your transcription.",
+      },
+      {
+        text: "τα και προστασσοντα εθνεσιν",
+      },
+      {
+        text: "εθνη α ουκ ηδεισαν σε επικα",
+      },
+      {
+        text: "λεσονται σε λαοι οι ουκ επισ",
+      },
+      {
+        text: "ταντα σε επι σε καταφευξον",
+      },
+      {
+        text: "ται εινεκεν του θυ σου του",
+      },
+      {
+        text: "αγιου ισραηλ οτι εδοξασεν",
+        caption:
+          "The hole on this line affected one whole letter and part of another. Notice the overline indicating this began a nomen sacrum; the following four letters should allow you to figure out the missing letters. Include them in your transcription.",
+        newConcept: "Reconstruction",
+      },
+      {
+        text: "σε ζητησατε τον θν και εν",
+      },
+    ],
+  },
+  6: {
     // helpful edition https://books.google.com/books?id=2c5hAAAAcAAJ&pg=PP118&lpg=PP118&dq=ephrem+syrus+%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82+%CE%BB%CE%BF%CE%B3%CE%BF%CF%82+%CE%B1&source=bl&ots=LeXDEgtDAt&sig=ACfU3U1qoHsw12pFlyLR5Tae8UnNklc6kg&hl=en&sa=X&ved=2ahUKEwif16uHiNH0AhX-Ap0JHUBGA_QQ6AF6BAgYEAM#v=onepage&q=ephrem%20syrus%20%CE%BA%CE%B1%CF%84%CE%B1%CE%BD%CE%B9%CE%BA%CF%84%CE%B9%CE%BA%CE%BF%CF%82%20%CE%BB%CE%BF%CE%B3%CE%BF%CF%82%20%CE%B1&f=false
     manifestId:
       "https://api.digitale-sammlungen.de/iiif/presentation/v2/bsb00050975/manifest",
@@ -300,11 +365,9 @@ const manifests: Manifests = {
       },
       {
         text: "ορθρου μι αναστασ επορευομην",
-        newConcept: "Ligature",
       },
       {
         text: "εγω συν τοισ δυο αδελφοισ εξωθεν",
-        newConcept: "Abbreviation",
       },
       {
         text: "τοισ πολεωσ εδεσων ευλογημενησ",
@@ -338,60 +401,6 @@ const manifests: Manifests = {
       },
       {
         text: "σθησ τησ φοβερασ εκεινησ ελευσεωσ",
-      },
-    ],
-  },
-  6: {
-    manifestId: "https://viewer.cbl.ie/viewer/api/v2/records/MP_2_86/manifest",
-    canvasIndex: 2,
-    specialIndexHandling: "pages/",
-    lines: [
-      {
-        text: "κουσατε μου και ζησεται ε",
-      },
-      {
-        text: "ν αγαθοισ η ψυχη υμων και",
-      },
-      {
-        text: "διαθησομαι υμιν διαθη",
-        newConcept: "Diaeresis",
-      },
-      {
-        text: "κην αιωνιον τα οσια δαυιδ",
-        caption:
-          "There's a correction on this line after the tau. It looks as if the scribe skipped a letter, starting writing the next word for two letters, then made the correction. Transcribe these two letters as α ο.",
-      },
-      {
-        text: "τα πιστα ιδου μαρτυριαν εν ε",
-      },
-      {
-        text: "θνεσιν αυτον δεδωκα αρχον",
-        caption:
-          "Another correction here by a later scribe; don't include it in your transcription.",
-      },
-      {
-        text: "τα και προστασσοντα εθνεσιν",
-      },
-      {
-        text: "εθνη α ουκ ηδεισαν σε επικα",
-      },
-      {
-        text: "λεσονται σε λαοι οι ουκ επισ",
-      },
-      {
-        text: "ταντα σε επι σε καταφευξον",
-      },
-      {
-        text: "ται εινεκεν του θυ σου του",
-      },
-      {
-        text: "αγιου ισραηλ οτι εδοξασεν",
-        caption:
-          "The hole on this line completely affected one letter and part of another. Notice the overline indicating this began a nomen sacrum. The following four letters should allow you to work out the missing letters.",
-        newConcept: "Reconstruction",
-      },
-      {
-        text: "σε ζητησατε τον θν και εν",
       },
     ],
   },
